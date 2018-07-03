274fed3d94b531c2c7d44c16d866c3de646809ad
rpeschke <richard.peschke@desy.de>
2018-07-02 14:50:56 -1000

    Files for Lecture 1

---
+++ Lecture01/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/home/ise/ise-shared/TestProject/TopLevelTb_isim_beh.exe" -prj "/home/ise/ise-shared/TestProject/TopLevelTb_beh.prj" "work.TopLevelTb" 
